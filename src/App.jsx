83f99add6adf8dc0381dc5dcf3b545c791a9afb4
sahajranipa <sahajranipa@gmail.com>
2024-10-07 13:50:33 +0530

    added store and it's reducer with it's config setups

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,50 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count] = useState(0);
+  const [input, setInput] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentId, setCurrentId] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  return <>Count: {count}</>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isEditing) {
+      setIsEditing(false);
+    } else {
+      console.log("Adding todo");
+    }
+    setInput("");
+  };
+  return (
+    <div>
+      <h1>Stackholic Todo App</h1>
+      <input
+        type="text"
+        placeholder="Search todo..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add todo..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">{isEditing ? "Save" : "Add"}</button>
+      </form>
+      <ul>
+        {todos.map((todo) => {
+          <li key={todo.id}>
+            <span>{todo.text}</span>
+            <button>Complete</button>
+            <button>Edit</button>
+            <button>Delete</button>
+          </li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/redux/todos/todoSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { v4 as uuidv4 } from "uuid";
+
+const initialState = {
+  todos: [],
+};
+
+const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: uuidv4(),
+        text: action.payload.text,
+        completed: false,
+      };
+      state.todos.push(newTodo);
+    },
+    toggleTodo: (state, action) => {
+      const index = state.todos.findIndex(
+        (todo) => todo.id === action.payload.id
+      );
+      if (index !== -1) {
+        state.todos[index].completed = !state.todos[index].completed;
+      }
+    },
+    updateTodo: (state, action) => {
+      const index = state.todos.findIndex(
+        (todo) => todo.id === action.payload.id
+      );
+      if (index !== -1) {
+        state.todos[index].text = action.payload.text;
+      }
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload.id);
+    },
+  },
+});
+
+export const { addTodo, toggleTodo, updateTodo, deleteTodo } =
+  todoSlice.actions;
+export default todoSlice.reducer;
